5dd444580048faeb0c7db7cd487bc4bf4f648e7c
phatvv <121276012+Moodyzhub@users.noreply.github.com>
2026-03-16 14:14:09 +0700

    up

---
+++ 71/src/constant/Message.java
@@ -0,0 +1,17 @@
+package constant;
+
+public final class Message {
+
+    private Message(){}
+    public static final String DELETE_SUCCESS ="Delete successfully!";
+    public static final String ADD_SUCCESS ="Add successfully!";
+    public static final String TASK_TYPE_INVALID ="Type must be 1-4";
+    public static final String DATE_INVALID ="Invalid date format!";
+    public static final String PLANT_TOFROM_INVALID ="To must be greater than from!";
+    public static final String ID_INVALID ="Id must be positive integer!";
+    public static final String TASK_NOT_FOUND ="Task not found  !";
+    public static final String NUMBER_INVALID ="Invalid number!";
+    public static final String TIME_INVALID ="Time must be between 8.0 to 17.5!";
+    public static final String STEP_INVALID ="Time must follow 0.5 step!";
+    public static final String INPUT_INVALID ="Input cant't be empty!";
+}
